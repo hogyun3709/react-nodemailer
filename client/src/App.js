5ffb242ba22700228a7bed597ceaea0fc37a0ccc
Hogyun Kim <hogyun3709@gmail.com>
2019-06-23 14:32:48 +0900

    Client: Create client side application: email send & confirm

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import Notification from "react-notify-toast";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Landing from "./components/Landing";
+import Confirm from "./components/Confirm";
+import Spinner from "./components/Spinner";
+import Footer from "./components/Footer/Footer";
+import { API_URL } from "./config";
+import "./App.css";
+
+export default class App extends Component {
+  /* Check server is running before the user interact with app */
+  state = {
+    loading: true
+  };
+
+  /* When using free hosting/deploying services, let the server keep running even no one use the application. */
+  componentDidMount = () => {
+    fetch(`${API_URL}/wake-up`)
+      .then(res => res.json())
+      .then(() => {
+        this.setState({ loading: false });
+      })
+      .catch(err => console.log(err));
+  };
+
+  render = () => {
+    /* Check out the server is running or not. If server runs, the landing component will be return instead a big spinner. */
+    const content = () => {
+      if (this.state.loading) {
+        return <Spinner size="10x" spinning="spinning" />;
+      }
+
+      return (
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/confirm/:id" component={Confirm} />
+            <Route exact path="/" component={Landing} />
+            <Redirect from="*" to="/" />
+          </Switch>
+        </BrowserRouter>
+      );
+    };
+
+    /* Container uses flex box */
+    /* The dom holds messages untill notifi.show is called*/
+    return (
+      <div className="container fadein">
+        <Notification />
+        <main>{content()}</main>
+        <Footer />
+      </div>
+    );
+  };
 }
-
-export default App;
---
+++ client/src/components/Landing.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { notify } from "react-notify-toast";
+import Spinner from "./Spinner";
+import { API_URL } from "../config";
+
+export default class Landing extends Component {
+  state = {
+    sendingEmail: false
+  };
+
+  onSubmit = event => {
+    /* Prevent a browser reload/refresh when the form submitted. It helps to keep the state condition */
+    event.preventDefault();
+    this.setState({ sendingEmail: true });
+
+    fetch(`${API_URL}/email`, {
+      method: "post",
+      headers: {
+        accept: "application/json",
+        "content-type": "applicaiton/json"
+      },
+      body: JSON.stringify({ email: this.email.value })
+    })
+      .then(res => res.json())
+      /* update the button and stop spinner, show toast msg, reset the form*/
+      .then(data => {
+        this.setState({ sendingEmail: false });
+        notify.show(data.msg);
+        this.form.reset();
+      })
+      .catch(err => console.log(err));
+  };
+
+  render = () => {
+    const { sendingEmail } = this.state;
+    /* ref is put on the form let reset the form after submission */
+    return (
+      <form onSubmit={this.onSubmit} ref={form => (this.form = form)}>
+        <div>
+          <input
+            type="email"
+            name="email"
+            ref={input => (this.email = input)}
+            required
+          />
+          <label htmlFor="email">Email</label>
+        </div>
+        <div>
+          {/* While email is being sent, disable the button by showing spinner */}
+          <button type="submit" className="btn" disabled={sendingEmail}>
+            {sendingEmail ? (
+              <Spinner size="lg" spinning="spinning" />
+            ) : (
+              "Send"
+            )}
+          </button>
+        </div>
+      </form>
+    );
+  };
+}
